bbf131ddb2c315c8ee360468f0fcdd5b3aeddec7
maxence <maxence.vasseur@epitech.eu>
2021-02-06 23:30:06 +0100

    Update List UI

--- lib/hero_vegetable_garden.dart
+++ lib/hero_vegetable_garden.dart
@@ -34,112 +34,117 @@ class _HeroGardenDataState extends State<HeroGardenData> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      height: MediaQuery.of(context).size.height / 4,
-      child: Hero(
-        tag: this.type + this.date,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height / 12 * 3,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Color(0xFF2ECC71),
-                ),
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      child: Image.asset(
-                        "assets/" + this.type + ".png",
-                        width: MediaQuery.of(context).size.width / 1.7,
-                        height: MediaQuery.of(context).size.height / 5,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF2ECC71),
+        elevation: 0,
+      ),
+      body: Container(
+        color: Colors.red,
+        child: Hero(
+          tag: this.type + this.date,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height / 12 * 3,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF2ECC71),
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        child: Image.asset(
+                          "assets/" + this.type + ".png",
+                          width: MediaQuery.of(context).size.width / 1.7,
+                          height: MediaQuery.of(context).size.height / 5,
+                        ),
+                        top: 20,
+                        left: -10,
                       ),
-                      top: 40,
-                      left: -10,
-                    ),
-                    Positioned(
-                      top: MediaQuery.of(context).size.height / 5,
-                      left: MediaQuery.of(context).size.width / 2,
-                      child: Text(
-                        this.type.toUpperCase(),
-                        style: TextStyle(
-                          color: Colors.black87,
-                          decoration: TextDecoration.none,
-                          fontSize: MediaQuery.of(context).size.width / 10,
+                      Positioned(
+                        top: MediaQuery.of(context).size.height / 5,
+                        left: MediaQuery.of(context).size.width / 2,
+                        child: Text(
+                          this.type.toUpperCase(),
+                          style: TextStyle(
+                            color: Colors.black87,
+                            decoration: TextDecoration.none,
+                            fontSize: MediaQuery.of(context).size.width / 10,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height / 12,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Color(0xFF2ECC71),
-                  borderRadius: new BorderRadius.only(
-                    bottomRight: const Radius.circular(240.0),
-                    bottomLeft: const Radius.circular(240.0),
+                    ],
                   ),
                 ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height / 12 * 6,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Container(
-                        child: Text(
-                          "Température: " + this.temp.toString(),
-                          style: TextStyle(
-                            decoration: TextDecoration.none,
-                            fontSize: 20,
-                            color: Colors.black
-                          ),
-                        ),
-                      ),
+                Container(
+                  height: MediaQuery.of(context).size.height / 12,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF2ECC71),
+                    borderRadius: new BorderRadius.only(
+                      bottomRight: const Radius.circular(240.0),
+                      bottomLeft: const Radius.circular(240.0),
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Container(
-                        child: Text(
-                          "Humidité: " + this.humidity.toString(),
-                          style: TextStyle(
-                            decoration: TextDecoration.none,
-                            fontSize: 20,
-                              color: Colors.black
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Container(
-                        child: Text(
-                          "Dernier Arrosage: " + this.date,
-                          style: TextStyle(
-                            decoration: TextDecoration.none,
-                            fontSize: 20,
-                              color: Colors.black
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  height: MediaQuery.of(context).size.height / 12 * 6,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Container(
+                          child: Text(
+                            "Température: " + this.temp.toString(),
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                fontSize: 20,
+                                color: Colors.black
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Container(
+                          child: Text(
+                            "Humidité: " + this.humidity.toString(),
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                fontSize: 20,
+                                color: Colors.black
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Container(
+                          child: Text(
+                            "Dernier Arrosage: " + this.date,
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                fontSize: 20,
+                                color: Colors.black
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
